33f890086410fbdadbe882f2331dceb976c3bb39
Benjamin Galan <bgalandiaz384@gmail.com>
2023-03-25 11:37:47 -0600

    Ej. predicado de prueba

--- Codelab_2/compNull.dart
+++ Codelab_2/compNull.dart
@@ -1,1 +1,11 @@
 // Operadores compatibles con nulos
+String? foo = 'a string';
+String? bar; // = null
+
+// Substitute an operator that makes 'a string' be assigned to baz.
+String? baz = foo ?? bar;
+
+void updateSomeVars() {
+  // Substitute an operator that makes 'a string' be assigned to bar.
+  bar ??= 'a string';
+}
